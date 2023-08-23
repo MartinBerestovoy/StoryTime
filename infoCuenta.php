eb1a7ccbedf36239c0bdb18b0248848b8f73b4f3
Martin Berestovoy <martin.berestovoy@gmail.com>
2023-08-23 10:37:52 -0300

    si

--- infoCuenta.php
+++ infoCuenta.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Perfil de Usuario</title>
-    <link rel="stylesheet" href="infoCuenta.css">
+    <link rel="stylesheet" href="styles/infoCuenta.css">
 </head>
 <body>
   <section class="perfil-container">
