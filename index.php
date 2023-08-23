a27fa3091346a7dc222127a6fd1d2ca093489497
Martin Berestovoy <martin.berestovoy@gmail.com>
2023-08-23 10:05:35 -0300

    orden, despues sigo

--- index.php
+++ index.php
@@ -1,3 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>Document</title>
+</head>
+<body>
+  <section id="inicio">
+    <img src="imgProyecto/logo.png" alt=""> <!--Logo de App-->
+    <h1 id="titulo">STORY TIME</h1>
+    <div>
+      <button><a href="inicioSesion.html" class="button">INICIA SESIÓN</a></button>
+    </div>
+    <br>
+    <div>
+      <button><a href="registro.html" class="button">REGISTRARSE</a></button>
+    </div>
+    <br>
+    <div>   
+      <button><a href="crearLibro.html" class="button"> INICIA COMO INVITADO </a></button>
+    </div>
+    <br>
+  </section>
+    
+</body>
+</html>
+
 <?php  
 $_ENV = parse_ini_file('.env');
 
