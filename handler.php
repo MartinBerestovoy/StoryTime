6c50932663469bcc9b8d1e58acc7eb56afc4190f
tomimoha <47349429@est.ort.edu.ar>
2023-09-05 11:02:43 -0300

    oracion cn info del form para mandarle a la ia

--- handler.php
+++ handler.php
@@ -1,3 +1,6 @@
 <?php 
+
 var_dump($_POST);
+$final_prompt = "Crea un libro el cual tenga como tematica " + $tematica[0] + ", que tenga de protagonista a " + $personaje[0] + " y que se lleve a cabo en " + $lugar[0];
+
 ?>
