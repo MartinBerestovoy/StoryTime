45a7c20c30786e6753cb7b55231a346e02b75c5d
tomimoha <47349429@est.ort.edu.ar>
2023-09-06 10:56:57 -0300

    avances en el for del array para del libro

--- handler.php
+++ handler.php
@@ -1,6 +1,14 @@
 <?php 
 
 var_dump($_POST);
-$final_prompt = "Crea un libro el cual tenga como tematica " + $tematica[0] + ", que tenga de protagonista a " + $personaje[0] + " y que se lleve a cabo en " + $lugar[0];
+
+$tematica = ["asd", "dasfaf"];
+
+for($i = 0; $i <= $tematica[].length(); $i++)
+{
+    $tematicasLibro = $i;
+}
+
+$final_prompt = "Crea un libro el cual tenga como tematica " + $tematicas + ", que tenga de protagonista a " + $personaje[0] + " y que se lleve a cabo en " + $lugar[0];
 
 ?>
