f53ec07ef6d102adc179b68eb94ede985a59c9e9
tomimoha <47349429@est.ort.edu.ar>
2023-09-07 10:00:57 -0300

    mini cambio del for

--- handler.php
+++ handler.php
@@ -3,9 +3,14 @@
 var_dump($_POST);
 
 
-// for($i = 0; $i <= count($tematicas[]); $i++)
+// for($i = 0; $i <= count($tematicas); $i++)
 // {
 //     $tematicas = $tematicas . $_POST;
+// }
+
+// for($i = 0; $i <= count($tematicas); $i++)
+// {
+//     $tematicas[$i] = $tematicas[$i] . $_POST;
 // }
 
 // $final_prompt = "Crea un libro el cual tenga como tematica " + $tematicas + ", que tenga de protagonista a " + $personajes + " y que se lleve a cabo en " + $lugares;
