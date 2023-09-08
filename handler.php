e74b89947a41113f80f5c76262ea114df60b3ccc
tomimoha <47349429@est.ort.edu.ar>
2023-09-08 10:01:34 -0300

    arreglos en el for, el promt y cambios en los values del form

--- handler.php
+++ handler.php
@@ -3,25 +3,26 @@
 var_dump($_POST);
 
 
-// for($i = 0; $i <= count($tematicas); $i++)
-// {
-//     $tematicasLibro = $tematicasLibro . $_POST[];
-// }
+/* for($i = 0; $i <= count($tematicas); $i++)
+ {
+     $tematicasLibro = $tematicasLibro . $_POST[];
+ }
 
-// for($i = 0; $i <= count($tematicas); $i++)
-// {
-//     $tematicas[$i] = $tematicas[$i] . $_POST;
-// }
+ for($i = 0; $i <= count($tematicas); $i++)
+ {
+     $tematicas[$i] = $tematicas[$i] . $_POST;
+}
+*/
 
 $tematicas = $_POST['tematicas'];
 $tematicasConcatenadas = "";
 
 for($i = 0; $i <= count($tematicas); $i++)
 {
-     $tematicasConcatenadas = $tematicasConcatenadas + ", " + $tematicas[$i];
+     $tematicasConcatenadas .= ", " . $tematicas[$i];
 }
 
 
-$final_prompt = "Crea un libro el cual tenga como tematica " + $tematicasConcatenadas + ", que tenga de protagonista a " + $personajesConcatenados + " y que se lleve a cabo en " + $lugaresConcatenados;
+$final_prompt = "Crea un cuento el cual tenga como tematica/s " . $tematicasConcatenadas . ", que tenga de protagonista/s a " . $personajesConcatenados . " y que se lleve a cabo en " . $lugaresConcatenados;
 
 ?>
