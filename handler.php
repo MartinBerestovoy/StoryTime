42a5ba7da544d3137460172ac9acfaa9f32f9342
EmmaMorvilloRPK <emmamorvillo@gmail.com>
2023-11-01 11:04:19 -0300

    funciona!!!! la api y la request (creo)

--- handler.php
+++ handler.php
@@ -92,29 +92,65 @@ $titulo_prompt = "Genera un titulo para el cuento";
 
 echo $final_prompt;
 
-// URL a la que deseas hacer la solicitud
-$url = 'https://api.openai.com/v1/chat/completions';
+// // URL a la que deseas hacer la solicitud
+// $url = 'https://api.openai.com/v1/chat/completions';
 
-// Inicializa una sesión cURL
-$ch = curl_init($url);
+// // Inicializa una sesión cURL
+// $ch = curl_init($url);
 
-// Establece opciones para la sesión cURL
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Hace que cURL devuelva la respuesta en lugar de imprimirla
+// // Establece opciones para la sesión cURL
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Hace que cURL devuelva la respuesta en lugar de imprimirla
 
-// Ejecuta la solicitud HTTP GET
+// // Ejecuta la solicitud HTTP GET
+// $response = curl_exec($ch);
+
+// // Verifica si hubo errores en la solicitud
+// if (curl_errno($ch)) 
+// {
+//     echo 'Error en la solicitud cURL: ' . curl_error($ch);
+// }
+
+// // Cierra la sesión cURL
+// curl_close($ch);
+
+// // Muestra la respuesta
+// echo $response;
+
+$_ENV = parse_ini_file(".env");
+
+$role = "¡Hola! Soy StoryBot, tu amigable contador de cuentos. ¿Estás listo para embarcarte en una aventura emocionante? Siéntate cómodamente y déjame llevarte a un mundo lleno de imaginación. En el mágico reino de las historias, donde los personajes cobran vida y los sueños se hacen realidad, estoy aquí para crear un cuento largo y creativo solo para ti. El cuento tiene que ser lo mas largo posible y tiene que tener un solo capitulo.";
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'https://api.openai.com/v1/chat/completions');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    'Content-Type: application/json',
+    'Authorization: Bearer ' . $_ENV["openai_api_key"],
+]);
+
+$data = [
+    'model' => 'gpt-3.5-turbo',
+    'messages' => [],
+];
+
+$data['messages'][] = ['role' => 'system', 'content' => $role];
+$data['messages'][] = ['role' => 'user', 'content' => $final_prompt];
+
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
 $response = curl_exec($ch);
 
-// Verifica si hubo errores en la solicitud
-if (curl_errno($ch)) 
-{
-    echo 'Error en la solicitud cURL: ' . curl_error($ch);
+$answer = "";
+
+$decoded_response = json_decode($response, true);
+if (isset($decoded_response['choices'][0]['message']['content'])) {
+    $answer = $decoded_response['choices'][0]['message']['content'];
+} else {
+    $answer = "Lorem ipsum dolor sit amet consectetur adipiscing, elit ante viverra nostra ornare. Placerat nisl bibendum sociosqu pulvinar euismod velit neque mattis nunc, luctus dui mus tristique nibh nulla nisi eget interdum nec, tempor class suspendisse dis maecenas quis rhoncus libero. Dui ultricies id aliquam elementum venenatis taciti sed tempus placerat sem, per integer lectus fusce vitae nascetur pharetra risus odio, curabitur natoque eu vestibulum interdum montes varius massa urna. Velit vel inceptos a luctus hendrerit rhoncus gravida ullamcorper libero, nibh malesuada suspendisse et penatibus aliquam tempor.";   
 }
 
-// Cierra la sesión cURL
 curl_close($ch);
 
-// Muestra la respuesta
-echo $response;
-
+echo $answer;
 
 ?>
