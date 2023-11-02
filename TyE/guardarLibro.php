80f4e619eef9def19d734bbebd60ea7f7109c644
tomimoha <47349429@est.ort.edu.ar>
2023-11-02 10:20:58 -0300

    guardar libros en BD

--- TyE/guardarLibro.php
+++ TyE/guardarLibro.php
@@ -1,11 +1,33 @@
 <?php
 
-include "textApi.php";
+//include "textApi.php";
+include "handler.php";
 include "conexionServer.php";
 error_reporting(0);
 session_start();
 
-$sql = "INSERT INTO biblioteca (titulo, text) VALUES ($title, $answer)"; //variables separadas o todo en una variable?
+$sql = "INSERT INTO biblioteca (text) VALUES ($response)";
 
+// $sql = "INSERT INTO biblioteca (text) VALUES (?)";
+
+// if ($stmt = $conn->prepare($sql)) 
+// {
+//     $stmt->bind_param("s", $response); 
+
+//     if ($stmt->execute()) 
+//     {
+//         echo "Datos insertados correctamente.";
+//     } 
+//     else 
+//     {
+//         echo "Error al ejecutar la sentencia preparada: " . $stmt->error;
+//     }
+
+//     $stmt->close();
+// } 
+// else 
+// {
+//     echo "Error al preparar la sentencia: " . $conn->error;
+// }
 
 ?>
