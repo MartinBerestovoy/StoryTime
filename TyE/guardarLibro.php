1b7b426fded18a1bf25f35808b558da96ef8c72e
EmmaMorvilloRPK <emmamorvillo@gmail.com>
2023-11-08 09:13:51 -0300

    no se

--- TyE/guardarLibro.php
+++ TyE/guardarLibro.php
@@ -1,33 +1,35 @@
 <?php
 
-//include "textApi.php";
+// include "textApi.php";
 include "handler.php";
 include "conexionServer.php";
 error_reporting(0);
 session_start();
 
-$sql = "INSERT INTO biblioteca (text) VALUES ($response)";
+$id_usuario = $_SESSION["id_usuario"];
 
-// $sql = "INSERT INTO biblioteca (text) VALUES (?)";
+$sql = "INSERT INTO biblioteca (text, id_usuario) VALUES ($response, $id_usuario)";
 
-// if ($stmt = $conn->prepare($sql)) 
-// {
-//     $stmt->bind_param("s", $response); 
 
-//     if ($stmt->execute()) 
-//     {
-//         echo "Datos insertados correctamente.";
-//     } 
-//     else 
-//     {
-//         echo "Error al ejecutar la sentencia preparada: " . $stmt->error;
-//     }
 
-//     $stmt->close();
-// } 
-// else 
-// {
-//     echo "Error al preparar la sentencia: " . $conn->error;
-// }
+ if ($stmt = $conn->prepare($sql)) 
+ {
+     $stmt->bind_param("s", $response); 
+
+     if ($stmt->execute()) 
+     {
+         echo "Datos insertados correctamente.";
+     } 
+     else 
+     {
+         echo "Error al ejecutar la sentencia preparada: " . $stmt->error;
+     }
+
+     $stmt->close();
+ } 
+ else 
+ {
+     echo "Error al preparar la sentencia: " . $conn->error;
+ }
 
 ?>
